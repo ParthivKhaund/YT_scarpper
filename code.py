5bbff0b09ea41b5419dcbe7ed8dfb9bd5bed92f8
Parthiv Khaund <parthivkhaund@gmail.com>
2020-05-15 07:02:43 -0400

    added comments and everythong , it works

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
-# selenium 
-# go to different videos 
-# scroll down 
-# call scrapper 
+# exception for sites with subscriber count hidden 
+# for some likes and dislikes get to sign in 
+# output to csv
+
 
 from bs4 import BeautifulSoup
 import requests 
@@ -9,7 +9,7 @@ import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-
+import time
 browser = webdriver.Chrome()
 
 title_list = []
@@ -19,12 +19,16 @@ subscriber_count_list = []
 views_list = []
 likes_list = []
 dislikes_list = []
+comments = []
 
 # # to clear cache 
 # browser.get('chrome://settings/clearBrowserData')
 # browser.find_element_by_xpath('//settings-ui').send_keys(Keys.ENTER)
+# browser.implicitly_wait(10)
 
-limit = 10
+limit = int(input("Enter nmber of sites : "))
+limit -= 1
+
 # scrapper function
 def scrapper_1(url):
 
@@ -68,6 +72,7 @@ def scrapper_1(url):
 
 	views = soup.find(class_="watch-view-count").get_text()
 	views_list.append(views)
+
 	# amount of likes and dislikes 
 
 	thumbs = soup.find_all(class_="yt-uix-button-content")
@@ -77,27 +82,43 @@ def scrapper_1(url):
 	likes = thumbs[-13].get_text()
 	likes_list.append(likes)
 
+
+# amount of comments 
+
+def comment_getter():
+	browser.execute_script('window.scrollTo(1, 500);')
+	time.sleep(5)
+	comment_div=browser.find_element_by_xpath('//*[@id="count"]/yt-formatted-string')
+	num_of_comments = comment_div.text
+	comments.append(num_of_comments)
+
 # starter Url
-#url = input("Please give start Url : ")
-url = "https://www.youtube.com/watch?v=qFkNATtc3mc"
+url = input("Please give start Url : ")
+# url = "https://www.youtube.com/watch?v=6FfD4Ims198"
 
 browser.get(url)
+browser.execute_script("window.scrollTo(0, 1000)") 
 
 # wait for all to load
-browser.implicitly_wait(10)
+# browser.implicitly_wait(10)
 
 # call scrapper 
 scrapper_1(url)
+comment_getter()
+browser.execute_script("window.scrollTo(0, 0)") 
+
+# for comments since they load later and need to scroll down 
 
 
 for i in range(limit):
 	browser.implicitly_wait(10)
 	next_url = browser.find_element_by_css_selector("#video-title")
 	next_url.click()
+
 	url = (browser.current_url)
 	scrapper_1(url)
-
-
+	comment_getter()
+	browser.execute_script("window.scrollTo(0, 0)") 
 
 # list output to be submitted to csv
 
@@ -108,3 +129,4 @@ print(subscriber_count_list)
 print(views_list)
 print(likes_list)
 print(dislikes_list)
+print(comments)
